04484ef06baa107db5edbfb82a1b9f9b03dd9b2e
Hafeza Mony <141733575+hafeza08mony@users.noreply.github.com>
2023-09-08 02:22:55 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        nav: true,
+        loop: true,
+    });
+  });
+
+  $(document).ready(function(){
+    $("brand-carousel").owlCarousel({
+        items:5,
+        nav: true,
+        loop: true,
+    });
+  });
